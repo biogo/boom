1d21061b4900202946abd934fc92149c0de7be05
kortschak <dan.kortschak@adelaide.edu.au>
2012-06-19 13:29:12 +0930

    Update bam example
    Demonstrate use of fetch.

--- examples/bamtest/bamtest.go
+++ examples/bamtest/bamtest.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"code.google.com/p/biogo.boom"
+	"fmt"
+)
+
+var (
+	chr   = "gi|166362741|ref|NC_010296.1|"
+	start = 3598704
+	end   = 3598705
 )
 
 func main() {
 	boom.Verbosity(0)
-	bf, err := boom.OpenBAM("test.bam")
+	bf, err := boom.OpenBAM("test-sort.bam")
 	if err != nil {
 		panic(err)
 	}
@@ -18,5 +24,25 @@ func main() {
 			break
 		}
 		fmt.Printf("%d %s %d-%d (%d) %v %d %d %s %v Fl:%v %q\n", r.ReferenceID(), r.Name(), r.Start(), r.End(), r.Len(), r.Cigar(), r.Strand(), r.Score(), r.Seq(), r.Quality(), r.Flags(), r.Tags())
+	}
+
+	fmt.Println("Fetch:", chr, start, end)
+
+	fn := func(r *boom.Record) {
+		fmt.Printf("%d %s %d-%d (%d) %v %d %d %s %v Fl:%v %q\n", r.ReferenceID(), r.Name(), r.Start(), r.End(), r.Len(), r.Cigar(), r.Strand(), r.Score(), r.Seq(), r.Quality(), r.Flags(), r.Tags())
+	}
+
+	i, err := boom.LoadIndex("test-sort.bam")
+	if err != nil {
+		panic(err)
+	}
+
+	if tid, ok := bf.GetTargetID(chr); ok {
+		_, err = bf.Fetch(i, tid, start, end, fn)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Printf("%q not present\n", chr)
 	}
 }
